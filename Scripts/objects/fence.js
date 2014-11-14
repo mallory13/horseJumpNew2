12c8442d4b7c4e44bd4d6e5624a23daf5f7c8eb6
mallory13 <mallory.steele13@gmail.com>
2014-11-14 11:48:00 -0500

    updated coordinates for fence class

--- Scripts/objects/fence.js
+++ Scripts/objects/fence.js
@@ -9,25 +9,28 @@ var objects;
             this.image = new createjs.Sprite(managers.Assets.atlas, "fence");
             this.width = this.image.getBounds().width;
             this.height = this.image.getBounds().height;
-            this.image.regX = this.width / 2;
-            this.image.regY = this.height / 2;
+            this.image.regX = this.width * 0.5;
+            this.image.regY = this.height * 0.5;
             this.reset();
 
             game.addChild(this.image);
         }
+		
+		//if the images reach the end of the canvas then reset the images by calling the reset function
         Fence.prototype.update = function () {
             this.image.y += this.dy;
             this.image.x += this.dx;
-            if (this.image.y > this.stage.canvas.height + this.height) {
+            if (this.image.x > this.stage.canvas.width + this.width) {
                 this.reset();
             }
         };
 
+//reset the images
         Fence.prototype.reset = function () {
-            this.image.x = Math.floor(Math.random() * this.stage.canvas.width);
-            this.dy = Math.floor(Math.random() * 5 + 5);
-            this.dx = Math.floor(Math.random() * -3) + Math.floor(Math.random() * 3);
-            this.image.y = -this.height;
+            this.image.y = Math.floor(Math.random() * this.stage.canvas.height);
+            this.dx = Math.floor(Math.random() * 5 + 5);
+            this.dy = Math.floor(Math.random() * -3) + Math.floor(Math.random() * 3);
+            this.image.x = -this.width;
         };
 
         Fence.prototype.destroy = function () {
