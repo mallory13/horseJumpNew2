3836089415f409ce1e768f9765adb2a3811d4b8b
mallory13 <mallory.steele13@gmail.com>
2014-11-14 10:53:14 -0500

    Downloaded boilerplate
    Commented where all changes need to be made

---
+++ Scripts/objects/island.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="../managers/asset.ts" />
+var objects;
+(function (objects) {
+    // Island Class
+	
+	//change island to hay?
+    var Island = (function () {
+        function Island(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Sprite(managers.Assets.atlas, "island");
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+			
+			//change to multiply instead of divide
+            this.image.regX = this.width / 2;
+            this.image.regY = this.height / 2;
+            this.reset();
+			
+			
+			//change to x
+            this.dy = 5;
+
+            game.addChild(this.image);
+        }
+        Island.prototype.update = function () {
+			
+			//change to negative width (x)
+            this.image.y += this.dy;
+            if (this.image.y > this.stage.canvas.height + this.height) {
+                this.reset();
+            }
+        };
+
+        Island.prototype.reset = function () {
+			
+			//change to height (y)
+            this.image.x = Math.floor(Math.random() * this.stage.canvas.width);
+			
+			//change to width (x)
+            this.image.y = -this.height;
+        };
+
+        Island.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+        return Island;
+    })();
+    objects.Island = Island;
+})(objects || (objects = {}));
+//# sourceMappingURL=island.js.map
---
+++ Scripts/objects/ocean.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../managers/asset.ts" />
+var objects;
+(function (objects) {
+    // Ocean Class
+	
+	//change ocean to field
+    var Ocean = (function () {
+        function Ocean(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(managers.Assets.loader.getResult("ocean"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.reset();
+
+			//change to dx
+            this.dy = 5;
+
+            game.addChild(this.image);
+        }
+		
+		//change ocean to field
+		//change y to x
+        Ocean.prototype.update = function () {
+            this.image.y += this.dy;
+            if (this.image.y >= 0) {
+                this.reset();
+            }
+        };
+
+		
+		//change ocean to field
+		//change y to x
+        Ocean.prototype.reset = function () {
+            this.image.y = -960;
+        };
+
+		//change ocean to field
+        Ocean.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+		
+		//change ocean to field
+        return Ocean;
+    })();
+    objects.Ocean = Ocean;
+})(objects || (objects = {}));
+//# sourceMappingURL=ocean.js.map
