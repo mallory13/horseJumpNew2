f4efa5aa02885a7432245b18a3e5f6aa49749d4d
mallory13 <mallory.steele13@gmail.com>
2014-11-14 11:04:20 -0500

    added field object

---
+++ Scripts/objects/field.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../managers/asset.ts" />
+var objects;
+(function (objects) {
+    // Field Class
+	
+	//change ocean to field
+    var Field = (function () {
+        function Field(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Bitmap(managers.Assets.loader.getResult("field"));
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.reset();
+
+			//change to dx
+            this.dy = 5;
+
+            game.addChild(this.image);
+        }
+		
+		//change ocean to field
+		//change y to x
+        Field.prototype.update = function () {
+            this.image.y += this.dy;
+            if (this.image.y >= 0) {
+                this.reset();
+            }
+        };
+
+		
+		//change ocean to field
+		//change y to x
+        Field.prototype.reset = function () {
+            this.image.y = -960;
+        };
+
+		//change ocean to field
+        Field.prototype.destroy = function () {
+            game.removeChild(this.image);
+        };
+		
+		//change ocean to field
+        return Field;
+    })();
+    objects.Field = Field;
+})(objects || (objects = {}));
+//# sourceMappingURL=ocean.js.map
